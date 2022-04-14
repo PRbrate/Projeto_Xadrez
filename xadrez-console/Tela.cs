fe1b239623e9e3c84ed282d4bfd351c1bb56aaae
Paulo Roberto <pauloteles08@hotmail.com>
2022-04-14 20:42:59 -0300

    fazendo os primeiros testes de movimentacao

--- xadrez-console/Tela.cs
+++ xadrez-console/Tela.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using tabuleiro;
+using Xadrez;
 
 namespace xadrez_console
 {
     class Tela
     {
-        public static void inprimirTabuleiro(Tabuleiro tab)
+        public static void imprimirTabuleiro(Tabuleiro tab)
         {
             for(int i =0; i<tab.linhas; i++)
             {
@@ -28,6 +29,15 @@ namespace xadrez_console
             }
             Console.WriteLine("  a b c d e f g h");
           
+        }
+
+        public static PosicaoXadrez lerPosicaoXadrex()
+        {
+            string s = Console.ReadLine();
+            char coluna= s[0];
+            int linha= int.Parse(s[1] + "");
+            return new PosicaoXadrez(coluna, linha);
+
         }
         public static void imprimirPeca(Peca peca)
         {
